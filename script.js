f14567bf4b3e28bbcea69e5f60a04c1b37bc9b7d
phantom2318 <25dcs095@charusat.edu.in>
2026-08-26 15:51:08 +0530

    initial

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMenu() 
+{
+    var menu = document.getElementById("menu-options");
+    if (menu.style.display === "none") 
+        {
+        menu.style.display = "block";
+        } 
+    else 
+        {
+        menu.style.display = "none";
+        }
+}
