835252b587f5a9261c46a53302f14ece0f550df1
phantom2318 <25dcs095@charusat.edu.in>
2026-09-17 14:57:03 +0530

    Registration page added

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function toggleMenu()
         menu.style.display = "none";
         }
 }
+
+// Define the API URL
+const apiUrl = 'https://home.openweathermap.org/api_keys';
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
